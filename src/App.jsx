f8b24489e0dbeaff7a72e264b3a2ffd9dcbaac99
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-04-27 13:08:13 +0200

    Added home/booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,67 @@
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Header } from './layout/Header';
+import { Footer } from './layout/Footer';
+
+export function Home() {
+  useEffect(() => {
+    document.title = 'Home page';
+  }, []);
+
+  return (
+    <>
+      <h2>Hello and welcome</h2>
+    </>
+  );
+}
+
+function Booking() {
+  useEffect(() => {
+    document.title = 'Booking page';
+  }, []);
+
+  return <div>Booking page</div>;
+}
+
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
+export function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/bookings">Booking</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
 export default function App() {
-  return <>
-  <h1 className="text-4xl border border-red-700">Hello</h1>
-  </>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="bookings" element={<Booking />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
