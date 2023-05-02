de72dde351bf285d8977a313f9f59272aadfd160
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-02 14:06:49 +0200

    Added API base urls

---
+++ src/services/Api/apiBase.js
@@ -0,0 +1,8 @@
+/* API Base urls */
+
+// This is the base url for the API of holidaze project
+export const baseUrl = 'https://api.noroff.dev/api/v1/holidaze/';
+
+export const getAllVenues = 'venues/';
+
+export const venuesId = 'id/';
