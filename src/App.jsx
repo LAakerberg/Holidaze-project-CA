653c68d74d2ed65dacead7e13a4855eac8a30f17
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-03 00:15:40 +0200

    Added Register/Auth & Login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Header } from './layout/Header';
 import { Footer } from './layout/Footer';
 import { Home } from './pages/HomePage/HomePage';
 import { Booking } from './pages/BookingPage/BookingPage';
-import { Register } from './pages/Register/RegisterPage';
+import { AuthUser } from './pages/AuthPage/Index';
+import { Register } from './pages/AuthPage/Register/RegisterPage';
+import { Login } from './pages/AuthPage/Login/LoginPage';
 
 function RouteNotFound() {
   return <div>Page not found</div>;
@@ -53,7 +55,9 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="bookings" element={<Booking />} />
-          <Route path="register" element={<Register />} />
+          <Route path="auth" element={<AuthUser />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
           <Route path="*" element={<RouteNotFound />} />
         </Route>
       </Routes>
