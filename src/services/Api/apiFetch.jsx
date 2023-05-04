b85a2608022a8cadf2299c46b8a0781853958739
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-04 12:28:49 +0200

    New API fetch code

--- src/services/Api/apiFetch.jsx
+++ src/services/Api/apiFetch.jsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from 'react';
-
 import { baseUrl, getAllVenues } from './apiBase';
-/* import { Card } from '../../components/Cards'; */
-import { ListVenues } from '../../components/VenuesList';
 
-export function FetchVenues() {
-  /*   const { id, name, price } = props; */
+export function useFetchVenues() {
   const [venues, setVenues] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    /* const { id, name, descriptions } = props */
     async function getVenues() {
       try {
         setIsError(false);
@@ -21,9 +16,10 @@ export function FetchVenues() {
         const response = await fetch(
           baseUrl + getAllVenues + '?limit=10_&offset=0'
         );
+
         const json = await response.json();
-        console.log(json);
         setVenues(json);
+
         setIsLoading(false);
       } catch (error) {
         setIsLoading(false);
@@ -34,32 +30,5 @@ export function FetchVenues() {
     getVenues();
   }, []);
 
-  if (isLoading) {
-    return <div>Loading Venues</div>;
-  }
-
-  if (isError) {
-    return <div>Error loading the venues</div>;
-  }
-
-  return (
-    <>
-      {/* <Card data={venues} /> */}
-      <ListVenues data={venues} />
-    </>
-  );
+  return { venues, isLoading, isError };
 }
-
-/* export function Card(props) {
-  return (
-    <>
-      {props.data.map((venue) => (
-        <div key={venue.id} className="bg-red-400 rounded-md p-1 m-1">
-          <div>{venue.id}</div>
-          <div>{venue.name}</div>
-          <div>{venue.description}</div>
-        </div>
-      ))}
-    </>
-  );
-} */
