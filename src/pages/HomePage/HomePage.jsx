1c2d90bc932e716bf37c53f3d0112a1c67ff3d1c
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-09 04:07:47 +0200

    Moved apiList

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { ApiCard } from '../../hooks/apiHook/cardHook';
 
 export function Home() {
   useEffect(() => {
@@ -8,12 +7,9 @@ export function Home() {
 
   return (
     <>
-      <main className="relative flex flex-col w-full md:w-4/5 max-w-screen-2xl px-1">
-        <div className="flex flex-row absolute -top-60 z-10 w-full p-2 gap-4 justify-center">
-          <ApiCard />
-        </div>
+      <main className="relative flex flex-col w-full md:w-4/5 max-w-screen-2xl px-1 z-10">
         <div className="mt-32"></div>
-        <div></div>
+        <div>hello</div>
       </main>
     </>
   );
