00115e89d998322185800953d8274f63d2f272ae
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-11 15:06:48 +0200

    Update App with correct style

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,8 @@ export function Main() {
   return (
     <>
       <main className="relative flex flex-col w-full md:w-4/5 max-w-screen-2xl px-1 z-10">
-        <div className="mt-32"></div>
+        <div className="mt-2"></div>
         <div className="">
-          <h3>Main</h3>
           <Outlet />
         </div>
       </main>
