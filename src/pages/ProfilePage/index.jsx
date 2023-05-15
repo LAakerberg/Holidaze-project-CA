a6305941f5d0467fea1b03afb3c403394d29f497
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-15 18:09:58 +0200

    Updated code with comments + re-trigger the API when delete venues

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,27 +1,41 @@
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useApiCall } from '../../hooks/api/useApiCall';
 import { getProfileUrl } from '../../services/authorization/apiBase';
 import { Profile } from '../../components/Profile';
 
+/**
+ * Profile page component.
+ * Displays user profile information.
+ */
 export function ProfilePage() {
-  const { name } = useParams();
+  const { name } = useParams(); // Get the 'name' parameter from the URL
+  const [shouldUpdateProfile, setShouldUpdateProfile] = useState(false); // State variable to trigger profile update
   const { data, isLoading, isError } = useApiCall(
-    getProfileUrl + name + `?_bookings=true&_venues=true`
+    getProfileUrl + name + `?_bookings=true&_venues=true`, // API endpoint for retrieving user profile data
+    shouldUpdateProfile // Pass shouldUpdateProfile as a dependency to trigger the API call
   );
 
-  const user = JSON.parse(localStorage.getItem('userData'));
+  const user = JSON.parse(localStorage.getItem('userData')); // Retrieve user data from localStorage
 
   useEffect(() => {
-    document.title = `Profile | ${user.name}`;
+    document.title = `Profile | ${user.name}`; // Set document title including the user's name
   }, []);
 
+  /**
+   * Event handler for venue deletion.
+   * Sets shouldUpdateProfile to true to trigger the API call and update the profile.
+   */
+  const handleVenueDelete = () => {
+    setShouldUpdateProfile(true);
+  };
+
   if (isLoading) {
-    return <div>Loading Profile</div>;
+    return <div>Loading Profile</div>; // Display loading message while profile data is being fetched
   }
 
   if (isError) {
-    return <div>Error loading the Profile</div>;
+    return <div>Error loading the Profile</div>; // Display error message if there was an error fetching the profile data
   }
 
   return (
@@ -30,7 +44,8 @@ export function ProfilePage() {
       <div className="p-1 my-1">
         <h3>Profile</h3>
         <div>
-          <Profile data={data} />
+          <Profile data={data} onVenueDelete={handleVenueDelete} />{' '}
+          {/* Render the Profile component with the fetched data and the delete venue event handler */}
         </div>
       </div>
     </>
