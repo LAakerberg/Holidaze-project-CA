e3798d12e654c8543a9077fe913b4f72a2e68b70
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-15 22:42:17 +0200

    new baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:4173/',
     // eslint-disable-next-line
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -16,10 +16,3 @@ export default defineConfig({
     },
   },
 });
-
-// Retry logic to set a fallback baseUrl
-// eslint-disable-next-line
-if (Cypress.config().baseUrl === 'http://localhost:3000') {
-  // eslint-disable-next-line
-  Cypress.config().baseUrl = 'http://localhost:4173/';
-}
