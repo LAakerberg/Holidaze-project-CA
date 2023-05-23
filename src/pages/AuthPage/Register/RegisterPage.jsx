5ca0fb30fc3b4569a8716127b39963751128859d
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-24 00:10:04 +0200

    removed main + changed some styles for the form

--- src/pages/AuthPage/Register/RegisterPage.jsx
+++ src/pages/AuthPage/Register/RegisterPage.jsx
@@ -3,28 +3,26 @@ import { RegistrationForm } from '../../../hooks/registration/RegistrationHook';
 export function Register() {
   return (
     <>
-      <main className="relative flex flex-col w-full md:w-4/5 max-w-screen-2xl px-1 py-6">
-        <div>
-          <h2>Register account</h2>
-        </div>
-        <div>
-          <div className="w-full border border-light_salmon p-1 mb-2 flex flex-col">
-            <div className="flex flex-row p-1">
-              <div className="flex-1">
-                <div>
-                  <RegistrationForm />
-                </div>
+      <div className="w-4/5 p-1 m-auto flex flex-col">
+        <h2>Register account</h2>
+      </div>
+      <div>
+        <div className="w-full lg:w-4/6 xl:w-3/6 border border-light_salmon rounded-lg p-1 m-auto flex flex-col bg-gray-200">
+          <div className="flex flex-row p-1">
+            <div className="flex-1">
+              <div>
+                <RegistrationForm />
               </div>
-              <div className="flex"></div>
             </div>
-            <div className="justify-center m-auto hidden">
-              <div className="text-center">
-                <button className="button primary">View</button>
-              </div>
+            <div className="flex"></div>
+          </div>
+          <div className="justify-center m-auto hidden">
+            <div className="text-center">
+              <button className="button primary">View</button>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
