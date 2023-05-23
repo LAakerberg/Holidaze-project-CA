383d30e27b1a99e629899edff9f9079c103fff46
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-24 00:13:20 +0200

    Moved the localStorage up in the code

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -9,14 +9,13 @@ import { Profile } from '../../components/Profile';
  * Displays user profile information.
  */
 export function ProfilePage() {
+  const user = JSON.parse(localStorage.getItem('userData')); // Retrieve user data from localStorage
   const { name } = useParams(); // Get the 'name' parameter from the URL
   const [shouldUpdateProfile, setShouldUpdateProfile] = useState(false); // State variable to trigger profile update
   const { data, isLoading, isError } = useApiCall(
     getProfileUrl + name + `?_bookings=true&_venues=true&sortOrder=asc`, // API endpoint for retrieving user profile data
     shouldUpdateProfile // Pass shouldUpdateProfile as a dependency to trigger the API call
   );
-
-  const user = JSON.parse(localStorage.getItem('userData')); // Retrieve user data from localStorage
 
   useEffect(() => {
     document.title = `Profile | ${user.name}`; // Set document title including the user's name
