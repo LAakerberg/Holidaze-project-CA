7c1e5a2f255015f5904ea73547ed91744819b06e
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-24 23:36:54 +0200

    cleaned up and seperetade calendar from the venueDetails file

--- src/components/venue/VenueDetails.jsx
+++ src/components/venue/VenueDetails.jsx
@@ -3,49 +3,18 @@ import { BsWifi } from 'react-icons/bs';
 import { TbParking } from 'react-icons/tb';
 import { BiRestaurant } from 'react-icons/bi';
 import { MdOutlinePets } from 'react-icons/md';
-import { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import { Calendar } from 'react-calendar';
-import { bookingVenueUrl } from '../../services/authorization/apiBase';
+import { BookingCalendar } from '../Calendar';
+import { renderDate } from '../../utils/formatDates';
 import 'react-calendar/dist/cjs/MonthView';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export function VenueDetails({ venueData }) {
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [bookedDates, setBookedDates] = useState([]);
-  console.log(venueData);
   console.log(venueData.bookings);
   const titleName = venueData.name;
 
   useEffect(() => {
     document.title = `Venue | ${titleName}`;
   }, []);
-
-  useEffect(() => {
-    if (venueData.bookings) {
-      const dates = venueData.bookings.reduce((acc, booking) => {
-        const startDate = new Date(booking.dateFrom);
-        const endDate = new Date(booking.dateTo);
-
-        const bookingDates = getBookingDates(startDate, endDate);
-        return [...acc, ...bookingDates];
-      }, []);
-
-      setBookedDates(dates);
-    }
-  }, [venueData.bookings]);
-
-  const getBookingDates = (startDate, endDate) => {
-    const dates = [];
-    const currentDate = new Date(startDate);
-
-    while (currentDate <= endDate) {
-      dates.push(new Date(currentDate));
-      currentDate.setDate(currentDate.getDate() + 1);
-    }
-
-    return dates;
-  };
 
   return (
     <>
@@ -162,97 +131,22 @@ export function VenueDetails({ venueData }) {
           <p>{venueData.description}</p>
         </div>
         <div className="border border-light_salmon m-1 p-1">
-          <p className="">Created: {venueData.created}</p>
+          <p className="">Created: {renderDate(venueData.created)}</p>
           <p className="">ID: {venueData.id}</p>
           <p className="">Max guests: {venueData.maxGuests}</p>
           <p className="">Price: {venueData.price}</p>
           <p className="">Rating: {venueData.rating}</p>
-          <p className="">Last update: {venueData.updated}</p>
+          <p className="">Last update: {renderDate(venueData.updated)}</p>
           <p className="">Location: {venueData.location?.city}</p>
         </div>
       </div>
       <div className="border border-light_salmon m-1 p-1">
         <h4>Select a Date for Booking</h4>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          minDate={new Date()}
-          excludeDates={bookedDates}
-        />
-      </div>
-      <div className="border border-light_salmon m-1 p-1">
-        <h4>Select a Date for Booking</h4>
-        <div className="text-center justify-center">
+        <div className="">
           <div>
             <BookingCalendar data={venueData} />
           </div>
         </div>
-      </div>
-    </>
-  );
-}
-
-function BookingCalendar({ data }) {
-  console.log(data.id);
-  const [selectedDates, setSelectedDates] = useState([]);
-
-  const handleDateChange = (selectedDate) => {
-    setSelectedDates(selectedDate);
-  };
-
-  const handleBookClick = async () => {
-    try {
-      if (selectedDates.length === 2) {
-        const dateFrom = selectedDates[0].toISOString();
-        const dateTo = selectedDates[1].toISOString();
-        const guests = 1;
-        const venueId = '9f1c16fe-6023-4af9-9c59-d3ec8863563f';
-
-        const accessToken = localStorage.getItem('accessToken');
-
-        // Perform API call here with the selected dates
-        const response = await fetch(bookingVenueUrl, {
-          method: 'POST',
-          body: JSON.stringify({ dateFrom, dateTo, guests, venueId }),
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        const responseData = await response.json();
-        console.log(responseData);
-
-        if (!response.ok) {
-          throw new Error('Failed to book. Please try again.');
-        }
-
-        const data = await response.json();
-        console.log(data);
-        // Handle API response
-      } else {
-        console.log('Please select both dateFrom and dateTo');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle error
-    }
-  };
-
-  return (
-    <>
-      <div>
-        <h1>Calendar Example</h1>
-        <Calendar
-          className="custom-calendar bg-gradient-to-b from-light_salmon to-topaz drop-shadow-lg"
-          onChange={handleDateChange}
-          value={selectedDates}
-          selectRange
-        />
-      </div>
-      <div>
-        <button className="button primary" onClick={handleBookClick}>
-          Book
-        </button>
       </div>
     </>
   );
