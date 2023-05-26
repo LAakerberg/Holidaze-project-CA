b6a77cd638ff98009329a27be4508d951c5ec393
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-26 20:58:42 +0200

    New import of login/reg message and redirect + new style

---
+++ src/components/Success.jsx
@@ -0,0 +1,53 @@
+import { Spinner } from './Spinner';
+import { useNavigate } from 'react-router-dom';
+
+/**
+ * Component for the success page after login.
+ * Navigates to the user's profile page.
+ */
+export function SuccessLogin() {
+  const user = JSON.parse(localStorage.getItem('userData'));
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate(`/profile/${user.name}`);
+  }, 200);
+  return (
+    <>
+      {' '}
+      <div className="border bg-green-500/50 border-green-800 w-full m-auto mt-5">
+        <div className="flex">
+          <div className="flex-1 p-1">
+            <p>Login was successful!</p>
+          </div>
+          <div className="flex-initial p-1">
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+/**
+ * Component for the success page after registration.
+ * Navigates to the login page.
+ */
+export function SuccessRegister() {
+  const navigate = useNavigate();
+  navigate(`/login`);
+  return (
+    <>
+      {' '}
+      <div className="border bg-green-500/50 border-green-800 w-full m-auto mt-5">
+        <div className="flex">
+          <div className="flex-1 p-1">
+            <p>Registration was successful</p>
+          </div>
+          <div className="flex-initial p-1">
+            <Spinner />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
