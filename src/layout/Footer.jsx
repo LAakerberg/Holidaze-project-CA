a9762fc087ae0c5f49a89f7012ce17ebf1946686
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-05-28 13:10:46 +0200

    Fixed wrong text

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,7 +6,7 @@ export function Footer() {
       <footer className="bg-light_salmon h-24 flex">
         <div className="m-auto flex">
           <div>
-            <p className="text-black">Holidazy Summer 2023 </p>
+            <p className="text-black">Holidaze Summer 2023 </p>
           </div>
           <div className="m-auto px-1">
             <AiOutlineCopyrightCircle />
